ef92d596706e8a6ec322eabecca9bc60cd2dc69d
Darryl Ha <darryl.ha@adroll.com>
2021-02-28 18:35:00 -0800

    Update dam_valuation.py

--- dam_valuation.py
+++ dam_valuation.py
@@ -26,9 +26,9 @@ income_data = requests.get(f'https://financialmodelingprep.com/api/v3/income-sta
 company_income_fund_date = next(item for item in income_data if item["date"] == "2019-12-31")
 
 balance_data = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?apikey={demo}').json()
-
 company_balance_fund_date = next(item for item in balance_data if item["date"] == "2019-12-31")
-
+enterprise_value_data = requests.get(f'https://financialmodelingprep.com/api/v3/enterprise-values/{company}?apikey={demo}').json()
+enterprise_value_date = next(item for item in enterprise_value_data if item["date"] == "2019-12-31")
 
 #inputs
 revenue_growth_rate = .4
@@ -37,8 +37,8 @@ steady_state_growth_rate_years = 5
 revenues = company_income_fund_date["revenue"]
 interest_expense = company_income_fund_date["interestExpense"]
 ebit = company_income_fund_date["operatingIncome"]
-sales_to_capital_ratio = 2
-
+sales_to_capital_ratio_initial = 2
+sales_to_capital_ratio_end = sales_to_capital_ratio_initial * .65
 
 #Years Vector 
 num_years_to_project = 11
@@ -62,39 +62,57 @@ ebit_margin = .016
 tax_rate = .25
 ebit_post_tax = ebit * ( 1 - tax_rate)
 speed_of_convergence = 5
-target_ebit_margin = .10
+target_ebit_margin = .1025
 ebit_margin_rate_years = pd.to_numeric(pd.Series(range(1,speed_of_convergence + 1)), downcast='float')
 ebit_margin_rate_convergence = target_ebit_margin - (target_ebit_margin - ebit_margin)/speed_of_convergence * (speed_of_convergence - ebit_margin_rate_years)
 ebit_margin_rate_proj[0] = ebit_margin 
 ebit_margin_rate_proj[1:len(ebit_margin_rate_years) + 1] = ebit_margin_rate_convergence
 ebit_margin_rate_proj[len(ebit_margin_rate_years) + 1:] = target_ebit_margin
+ebit_proj = ebit_margin_rate_proj * revenues_proj
+ebit_post_tax_proj = ebit_proj * (1 - tax_rate)
+ebit_post_tax_proj[ebit_post_tax_proj < 0] = 0
+
+
 
 #Sales to Capital Ratio
-
-copy.deepcopy(years_proj)
-
-
-
+sales_to_capital_series = copy.deepcopy(years_proj)
+sales_to_capital_series[1:6] = sales_to_capital_ratio_initial
+sales_to_capital_series[6:] = sales_to_capital_ratio_end
 
 
+#fcff
+capex_proj = revenues_proj.diff() / sales_to_capital_series
+fcff_proj = (ebit_post_tax_proj - capex_proj).dropna()
 
 
-cagr = 1
-discount_rate = .1
-LTGrowth = .02
+#Discount Factor
+cum_discount_factor_proj = copy.deepcopy(years_proj)
+risk_free_rate = .0175
+initial_cost_of_capital = .0886
+mature_cost_of_capital = .074
+cost_of_capital_vec = pd.Series(range(0,num_years_to_project), dtype = 'float64')
+cost_of_capital_vec[1:6] = initial_cost_of_capital
+cost_of_capital_vec[6:] = mature_cost_of_capital - (mature_cost_of_capital - initial_cost_of_capital)/speed_of_convergence * (speed_of_convergence - pd.Series(range(1,6), dtype = 'float64'))
 
-pd.DataFrame(np.random.randn(7, 2))
+cum_discount_factor_proj = (1/(1 + cost_of_capital_vec)).cumprod()
 
-tax_rate = .25
-ebit_post_tax = ebit * ( 1 - tax_rate)
-sales_to_capital_ratio = 2
-revenue_growth_rate = .4 
-
-cost_of_capital = .08
+pv_fcff = cum_discount_factor_proj[:-1].reset_index(drop=True) * fcff_proj[:-1].reset_index(drop=True)
 
 
-a_list = [1, 3, 49, 23, 4, 23]
-def condition(x): return x > 4
-output = [idx for idx, element in enumerate(a_list) if condition(element)]
-print(output)
-
+terminal_cash_flow = fcff_proj.iloc[-1]
+terminal_cost_of_capital = cost_of_capital_vec.iloc[-1]
+terminal_growth_rate = revenue_growth_rate_series.iloc[-1] - 1
+terminal_value = terminal_cash_flow/(terminal_cost_of_capital - terminal_growth_rate)
+pv_terminal_value = terminal_value * cum_discount_factor_proj.iloc[-1]
+pv_cf_sum = sum(pv_fcff)
+pv_sum = pv_cf_sum + pv_terminal_value
+failure_prob = .1
+fair_value_weight = .5
+proceeds_of_failure = pv_sum * fair_value_weight
+operating_asset_value = pv_sum * (1 - failure_prob) + proceeds_of_failure * failure_prob
+cash = company_balance_fund_date["cashAndCashEquivalents"]
+debt = company_balance_fund_date["totalDebt"]
+equity_value = operating_asset_value + cash - debt
+outstanding_shares = enterprise_value_date["numberOfShares"]
+equity_value
+print(f"{equity_value:,}")
